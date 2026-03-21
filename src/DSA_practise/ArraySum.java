2e7db9e0827ed10eaa5441312e0272eb2d8898fd
Ananya G Bharadwaj <ananyagbharadwaj732005@gmail.com>
2026-03-21 21:38:13 +0530

    Update ArraySum.java

--- src/DSA_practise/ArraySum.java
+++ src/DSA_practise/ArraySum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class array {
+class Array {
     public static void main(String[] args) {
         System.out.println("Enter the number of elements");
         Scanner sc = new Scanner(System.in);
