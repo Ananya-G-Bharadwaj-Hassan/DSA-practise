6d9901fd52fb7b72ffca1f5fdd97a6fc0086567e
AnanyaGBharadwaj <ananyagbharadwaj@gmail.com>
2026-03-27 21:15:53 +0530

    4

---
+++ src/DSA_practise/Subarrays.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class subarrays{
+    public static void main(String[] args) {
+        System.out.println("Enter the number of elements in the array");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+
+        System.out.println("Enter the elements");
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        for(int si=0;si<n;si++){
+            for(int ei=si;ei<n;ei++){
+                for(int k=si;k<=ei;k++){
+                    System.out.print(arr[k]);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
