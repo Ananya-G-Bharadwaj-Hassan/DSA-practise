5c28a571e2457717e1ada929df3dc6cee30e2a75
Ananya G Bharadwaj <ananyagbharadwaj732005@gmail.com>
2026-03-30 14:25:49 +0530

    Update SumOfMinMax.java

--- src/DSA_practise/SumOfMinMax.java
+++ src/DSA_practise/SumOfMinMax.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class minmaxarray {
+class MinMaxArray {
     public static void main(String[] args) {
         System.out.println("Enter the number of elements");
         Scanner sc = new Scanner(System.in);
