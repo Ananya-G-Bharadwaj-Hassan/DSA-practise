f9bf50aa0f26329fe3e2d214ecbb8611fffe26b7
Ananya G Bharadwaj <ananyagbharadwaj732005@gmail.com>
2026-04-01 22:06:12 +0530

    Update LinearSearch.java

--- src/DSA_practise/LinearSearch.java
+++ src/DSA_practise/LinearSearch.java
@@ -5,7 +5,7 @@ class Main {
         System.out.println(search(5));
     }
 
-    static int Search(int n) {
+    static int search(int n) {
         int[] arr = {1, 2, 3, 4, 5, 6};
 
         for (int i = 0; i < arr.length; i++) {
