2827475761fb5b2b0c297a8a61375c1d0fb892a5
Ananya G Bharadwaj <ananyagbharadwaj732005@gmail.com>
2026-04-05 22:50:03 +0530

    Update RemoveVowels.java

--- src/DSA_practise/RemoveVowels.java
+++ src/DSA_practise/RemoveVowels.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-class vowel {
+class Vowel {
     public static void main(String[] args) {
         String s="Hello";
         removevowel(s);
     }
 
-    static void removevowel(String s){
+    static void RemoveVowels(String s){
         for (int i = 0; i < s.length(); i++) {
             char Ch = s.charAt(i);
             if (Ch == 'a' || Ch == 'e' || Ch == 'i' || Ch == 'o' || Ch == 'u') {
