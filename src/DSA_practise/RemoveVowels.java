feef59691f14ffd3e40520b2b02d1ad0c430aa90
Ananya G Bharadwaj <ananyagbharadwaj732005@gmail.com>
2026-04-09 19:46:44 +0530

    Update RemoveVowels.java

--- src/DSA_practise/RemoveVowels.java
+++ src/DSA_practise/RemoveVowels.java
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 package DSA_practise;
 
-class vowel {
-=======
 import java.util.Scanner;
 class Vowel {
->>>>>>> 2827475761fb5b2b0c297a8a61375c1d0fb892a5
     public static void main(String[] args) {
         String s="Hello";
         removevowel(s);
