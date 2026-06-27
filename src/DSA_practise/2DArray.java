5d0d2a0d13b3ad6f5c015f42a53910915c291fcf
Ananya G Bharadwaj <ananyagbharadwaj732005@gmail.com>
2026-06-27 22:20:20 +0530

    Update 2DArray.java

--- src/DSA_practise/2DArray.java
+++ src/DSA_practise/2DArray.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-class Twodimensionalarray{
+class TwoDimensionalArray{
     public static void main(String[] args) {
         int[][] arr={
                 {10,20,30,40},
