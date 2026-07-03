f87249d803c027b5bef1c6fa7e73bd961dbf8066
AnanyaGBharadwaj <ananyagbharadwaj@gmail.com>
2026-07-03 22:07:08 +0530

    final

---
+++ src/DSA_practise/Factorial.java
@@ -0,0 +1,13 @@
+class factorial{
+    public static void main(String[] args) {
+       System.out.print(factorial(5));
+    }
+    public static int factorial(int n){
+        if (n==0) {
+            return 1;
+        }
+        else{
+            return n*factorial(n-1);
+        }
+    }
+}
