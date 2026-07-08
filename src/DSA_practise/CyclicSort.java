c4c28acd3e828e0d643d6a7bf487c12c872aec04
AnanyaGBharadwaj <ananyagbharadwaj@gmail.com>
2026-07-08 21:50:26 +0530

    cyclic

---
+++ src/DSA_practise/CyclicSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class cyclicsort{
+    public static void main(String[] args) {
+        int[] arr={1,4,2,3,5};
+        cyclicsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void cyclicsort(int[] arr){
+            int i=0;
+            while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }else{
+                i++;
+            }
+        }
+    }
+}
