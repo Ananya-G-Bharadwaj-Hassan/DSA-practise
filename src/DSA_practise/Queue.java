ba8aad0d8176e009cbd7962cea871fef2044f5d8
Ananya G Bharadwaj <ananyagbharadwaj732005@gmail.com>
2026-07-25 22:10:05 +0530

    Update Queue.java

--- src/DSA_practise/Queue.java
+++ src/DSA_practise/Queue.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Queue;
 
-class Queue{
+class Queues{
    public static void main(String[] args) {
        Queue<Integer> queue = new ArrayDeque<>();
 
