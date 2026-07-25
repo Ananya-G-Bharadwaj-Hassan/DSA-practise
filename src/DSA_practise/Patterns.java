cf9c9ca4352fdf058d9180a666557c4819610e23
AnanyaGBharadwaj <ananyagbharadwaj@gmail.com>
2026-07-25 22:07:52 +0530

    pattern

--- src/DSA_practise/Patterns.java
+++ src/DSA_practise/Patterns.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-class patterns{
+class patterns {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n");
         int n = sc.nextInt();
 
         //pattern 1
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print(" *");
             }
             System.out.println();
@@ -16,8 +16,8 @@ class patterns{
 
         System.out.println();
         //pattern 2
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
                 System.out.print(" *");
             }
             System.out.println();
@@ -25,8 +25,8 @@ class patterns{
 
         System.out.println();
         //pattern 3
-        for(int i=0;i<n;i++){
-            for(int j=n-i-1;j<n;j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i - 1; j < n; j++) {
                 System.out.print(" *");
             }
             System.out.println();
@@ -34,18 +34,18 @@ class patterns{
 
         System.out.println();
         //pattern 4
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j+" ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
 
         System.out.println();
         //pattern 5
-        for(int i=0;i<2*n;i++){
-            int totalcolsinrow=i>n?2*n-i:i;
-            for(int j=0;j<totalcolsinrow;j++){
+        for (int i = 0; i < 2 * n; i++) {
+            int totalcolsinrow = i > n ? 2 * n - i : i;
+            for (int j = 0; j < totalcolsinrow; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -53,13 +53,13 @@ class patterns{
 
         System.out.println();
         //pattern 6
-        for(int i=0;i<2*n;i++){
-            int totalColsInRow=i>n?2*n-i:i;
-            int spaces=n-totalColsInRow;
-            for(int k=0;k<spaces;k++){
+        for (int i = 0; i < 2 * n; i++) {
+            int totalColsInRow = i > n ? 2 * n - i : i;
+            int spaces = n - totalColsInRow;
+            for (int k = 0; k < spaces; k++) {
                 System.out.print(" ");
             }
-            for(int j=0;j<totalColsInRow;j++){
+            for (int j = 0; j < totalColsInRow; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -67,18 +67,45 @@ class patterns{
 
         System.out.println();
         //pattern 7
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
 
-            for(int k=0;k<n-i;k++){
+            for (int k = 0; k < n - i; k++) {
                 System.out.print(" ");
             }
-            for(int j=i;j>=1;j--){
+            for (int j = i; j >= 1; j--) {
                 System.out.print(j);
             }
-            for(int j=2;j<=i;j++){
+            for (int j = 2; j <= i; j++) {
                 System.out.print(j);
             }
             System.out.println();
         }
+
+        System.out.println();
+        //pattern 8
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        //pattern 9
+        n = 2 * n;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                int atEveryIdx = Math.min(Math.min(i, j), Math.min(n - i, n - j));
+                System.out.print(atEveryIdx);
+            }
+            System.out.println();
+        }
+
     }
 }
