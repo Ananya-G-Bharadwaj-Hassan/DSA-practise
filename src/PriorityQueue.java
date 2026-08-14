1a5a032f110e49120f88fe0c45104ce025eea0dc
Ananya-G-Bharadwaj-Hassan <ananyagbharadwaj732005@gmail.com>
2026-08-14 22:12:15 +0530

    priority queue

---
+++ src/PriorityQueue.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+
+class pq{
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        //to change the priority do
+        // PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+
+        //put(O(logn))
+        pq.add(10);
+        pq.add(20);
+        pq.add(30);
+        pq.add(10);
+        System.out.println(pq);
+
+        //get(O(logn))
+        System.out.println(pq.remove());
+        System.out.println(pq.remove());
+
+        //peek(O(1))
+        System.out.println(pq.peek());
+
+        //size(O(1))
+        System.out.println(pq.size());
+
+        //empty
+        System.out.println(pq.isEmpty());
+    }
+}
