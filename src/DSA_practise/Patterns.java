b21e1d0338c6fb4378f3ad75995310e792d035ff
Ananya-G-Bharadwaj-Hassan <ananyagbharadwaj732005@gmail.com>
2026-08-21 21:41:25 +0530

    pattern 10

--- src/DSA_practise/Patterns.java
+++ src/DSA_practise/Patterns.java
@@ -107,5 +107,16 @@ class patterns {
             System.out.println();
         }
 
+        System.out.println();
+        //pattern 10
+        for(int i=1;i<=n;i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
